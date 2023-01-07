949c582c59ab73d12f5c352d83cfba36f6f99036
Dmytro Stetsenko <d.a.stetsenko@gmail.com>
2023-01-07 21:18:32 +0200

    base markup posts and create screen

--- screens/PostsScreen.jsx
+++ screens/PostsScreen.jsx
@@ -1,16 +1,50 @@
-import React from "react";
-import {StyleSheet,Text,View} from "react-native"
-export default function PostsScreen() {
-    return (
-        <View style={styles.bg}>
-        <Text style={{fontSize:45}}>Posts Screen</Text>
-        </View>
-    )
-}
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, FlatList, Image, Button } from "react-native";
+
+const DefaultScreenPosts = ({ route, navigation }) => {
+  const [posts, setPosts] = useState([]);
+  console.log("route.params", route.params);
+
+  useEffect(() => {
+    if (route.params) {
+      setPosts((prevState) => [...prevState, route.params]);
+    }
+  }, [route.params]);
+  console.log("posts", posts);
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item, indx) => indx.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              marginBottom: 10,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={{ uri: item.photo }}
+              style={{ width: 350, height: 200 }}
+            />
+          </View>
+        )}
+      />
+      <Button title="go to map" onPress={() => navigation.navigate("Map")} />
+      <Button
+        title="go to Comments"
+        onPress={() => navigation.navigate("Comments")}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    bg: {
-        flex:1,
-    backgroundColor: "#FFFFFF",
-}
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
+
+export default DefaultScreenPosts;
